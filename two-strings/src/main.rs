1bfa5e1407f35431b9e440895a18950652c2775d
Alexander Korolev <kilork@yandex.ru>
2018-05-06 01:58:08 +0300

    two-strings

---
+++ two-strings/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let count: usize = read_line().parse().unwrap();
+    for _ in 0..count {
+        let result = process_two_strings();
+        println!("{}", result);
+    }
+}
+
+fn process_two_strings() -> &'static str {
+    let mut line1 : Vec<u8> = read_line().bytes().collect();
+    line1.sort_unstable();
+    line1.dedup();
+    let mut line2 : Vec<u8> = read_line().bytes().collect();
+    line2.sort_unstable();
+    for b in line1 {
+        if let Ok(_) = line2.binary_search(&b) {
+            return "YES"
+        }
+    }
+    "NO"
+}
+
+fn read_line() -> String {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().to_string()
+}
